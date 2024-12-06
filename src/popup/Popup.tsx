93cd90479e35cd3405e1f580eee1d676b37dadf7
Mich Ochieng (CNC) <michochieng@gmail.com>
2024-12-06 12:38:53 -0800

    style: added more styling to buttons and heading, also added functuions for buttons

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,20 +1,31 @@
 import React, { JSX } from 'react';
 
+import { handleEdit, handlePreview } from '../background/index';
+
 export default function Popup(): JSX.Element {
   return (
     <div id='my-ext' className='container' data-theme='light'>
-      <button type='button' className='btn btn-outline'>
-        Default
-      </button>
-      <button type='button' className='btn btn-outline btn-primary'>
-        Primary
-      </button>
-      <button type='button' className='btn btn-outline btn-secondary'>
-        Secondary
-      </button>
-      <button type='button' className='btn btn-outline btn-accent'>
-        Accent
-      </button>
+      <div className='grid h-[100px] w-[250px] grid-cols-2 grid-rows-3 gap-4'>
+        {/* Heading */}
+        <div className='col-span-2 text-center'>
+          <h1 className='font-medium'>Sitefinity Actions</h1>
+        </div>
+        {/* Button group */}
+        <button
+          onClick={handleEdit}
+          type='button'
+          className='btn btn-outline col-span-1'
+        >
+          Edit
+        </button>
+        <button
+          onClick={handlePreview}
+          type='button'
+          className='btn btn-outline btn-primary col-span-1'
+        >
+          Preview
+        </button>
+      </div>
     </div>
   );
 }
